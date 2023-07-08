f63f2247294a25370e099413a68ef0a0fc057de5
Symbat Zinatova <symbatzinatova.k@gmail.com>
2023-07-08 20:08:49 +0600

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/symbatDeveloper/git-epam.git";
+    public static String REPO_LINK = "https://github.com/k-allard/git_training.git";
 }
